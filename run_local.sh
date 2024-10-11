81a473802f9d011fb8e6fd8b5670173211a170d7
root <root@sdlc.us-central1-f.c.alpine-scholar-358023.internal>
2024-10-11 05:09:55 +0000

    new changes

--- run_local.sh
+++ run_local.sh
@@ -1,7 +1,7 @@
-export git_dir_name=<GIT_PROJECT_NAME>
-export GIT_USERNAME=<GIT_USERNAME>
-export GIT_PASSWORD=<GIT_PAT>
-export git_remote_url=https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/<Your_GIT_Name>/${git_dir_name}.git
+export git_dir_name=JIRA-GPT
+export GIT_USERNAME=lagairogo@gmail.com
+export GIT_PASSWORD=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
+export git_remote_url=https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/lagairogo-gai/${git_dir_name}.git
 
 streamlit run main.py --server.port=8090 --server.address=0.0.0.0
 
